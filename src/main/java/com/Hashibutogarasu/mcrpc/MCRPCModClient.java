baa1c077a22532d9c53044cd884d6406b9a7e7ba
crow <corvusmacrorhynchosjaponensis@gmail.com>
2022-02-13 15:55:46 +0900

    いらんファイルを削除して変更した

--- src/main/java/com/Hashibutogarasu/mcrpc/MCRPCModClient.java
+++ src/main/java/com/Hashibutogarasu/mcrpc/MCRPCModClient.java
@@ -1,25 +1,14 @@
 package com.Hashibutogarasu.mcrpc;
 
-import com.Hashibutogarasu.mcrpc.Config.ClothConfigScreenFactory;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MCRPCModClient implements ClientModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+public class MCRPCMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("MCRPC");
 
-    @Override
-    public void onInitializeClient() {
+	@Override
+	public void onInitialize() {
+
     }
 }
-
-
